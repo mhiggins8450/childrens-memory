ebdda3fc13791d602db45d2aac6726fd1b9384c7
mhiggins8450 <146852125+mhiggins8450@users.noreply.github.com>
2023-11-08 01:24:58 -0500

    Recovered shuffling capability in Start button

--- script.js
+++ script.js
@@ -1,69 +1,10 @@
-// document.addEventListener("DOMContentLoaded", function () {
-//   const blocks = document.querySelectorAll(".block");
-//   let firstBlock = null;
-//   let secondBlock = null;
-//   let shuffled = false;
-
-//   blocks.forEach((block) => {
-//     block.addEventListener("click", () => {
-//       if (!shuffled) {
-//         return; // Prevent clicking on blocks before shuffling
-//       }
-
-//       if (!firstBlock) {
-//         firstBlock = block;
-//         firstBlock.classList.add("active");
-//       } else if (!secondBlock) {
-//         secondBlock = block;
-//         secondBlock.classList.add("active");
-
-//         // Check for a match
-//         if (
-//           firstBlock.querySelector("img").getAttribute("src") ===
-//           secondBlock.querySelector("img").getAttribute("src")
-//         ) {
-//           // If it's a match, keep the blocks active
-//           firstBlock = null;
-//           secondBlock = null;
-//         } else {
-//           // If it's not a match, hide the images
-//           setTimeout(() => {
-//             firstBlock.classList.remove("active");
-//             secondBlock.classList.remove("active");
-//             firstBlock = null;
-//             secondBlock = null;
-//           }, 1000);
-//         }
-//       }
-//     });
-//   });
-
-//   const startButton = document.getElementById("start-button");
-//   startButton.addEventListener("click", () => {
-//     // Shuffle the blocks/images
-//     blocks.forEach((block) => {
-//       block.classList.remove("active");
-//     });
-//     shuffleBlocks();
-//     shuffled = true;
-//   });
-
-//   function shuffleBlocks() {
-//     blocks.forEach((block) => {
-//       block.style.order = Math.floor(Math.random() * blocks.length);
-//     });
-//   }
-// });
-
-//ADDING A RESET BUTTON BELOW
-
 document.addEventListener("DOMContentLoaded", function () {
   const blocks = document.querySelectorAll(".block");
   let firstBlock = null;
   let secondBlock = null;
+  let shuffled = false;
 
   const resetButton = document.getElementById("reset-button");
-  const startButton = document.getElementById("start-button");
 
   resetButton.addEventListener("click", () => {
     blocks.forEach((block) => {
@@ -71,12 +12,12 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   });
 
-  startButton.addEventListener("click", () => {
-    // Shuffle the game or start a new game
-  });
-
   blocks.forEach((block) => {
     block.addEventListener("click", () => {
+      if (!shuffled) {
+        return; // Prevent clicking on blocks before shuffling
+      }
+
       if (!firstBlock) {
         firstBlock = block;
         firstBlock.classList.add("active");
@@ -104,4 +45,20 @@ document.addEventListener("DOMContentLoaded", function () {
       }
     });
   });
+
+  const startButton = document.getElementById("start-button");
+  startButton.addEventListener("click", () => {
+    // Shuffle the blocks/images
+    blocks.forEach((block) => {
+      block.classList.remove("active");
+    });
+    shuffleBlocks();
+    shuffled = true;
+  });
+
+  function shuffleBlocks() {
+    blocks.forEach((block) => {
+      block.style.order = Math.floor(Math.random() * blocks.length);
+    });
+  }
 });
