cb97490577c45dd2bd45ecdea3c4856f06036d1d
mhiggins8450 <146852125+mhiggins8450@users.noreply.github.com>
2023-11-08 15:04:51 -0500

    Successfully implemented 4 x 3 multiplayer game area

--- script.js
+++ script.js
@@ -1,64 +1,104 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const blocks = document.querySelectorAll(".block");
+  const playerArea = document.getElementById("player-area");
+  const computerArea = document.getElementById("computer-area");
+  const startButton = document.getElementById("start-button");
+  const resetButton = document.getElementById("reset-button");
+  let shuffled = false;
   let firstBlock = null;
   let secondBlock = null;
-  let shuffled = false;
+  const images = [
+    "images/deer.jpg",
+    "images/fox.jpg",
+    "images/giraffe.jpg",
+    "images/hedgehog.jpg",
+    "images/polarbear.jpg",
+    "images/rabbit.jpg",
+    "images/deer.jpg",
+    "images/fox.jpg",
+    "images/giraffe.jpg",
+    "images/hedgehog.jpg",
+    "images/polarbear.jpg",
+    "images/rabbit.jpg",
+  ];
 
-  const resetButton = document.getElementById("reset-button");
+  // Shuffle the images array
+  for (let i = images.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [images[i], images[j]] = [images[j], images[i]];
+  }
+
+  // Duplicate each image path to ensure they are used twice
+  const duplicatedImagePaths = [...images, ...images];
+
+  startButton.addEventListener("click", () => {
+    if (!shuffled) {
+      playerArea.innerHTML = "";
+      computerArea.innerHTML = "";
+      createBlocks(playerArea, 12);
+      createBlocks(computerArea, 12);
+      shuffleBlocks(playerArea);
+      shuffleBlocks(computerArea);
+      shuffled = true;
+    }
+  });
 
   resetButton.addEventListener("click", () => {
-    blocks.forEach((block) => {
-      block.classList.remove("active");
-    });
+    playerArea.innerHTML = "";
+    computerArea.innerHTML = "";
+    shuffled = false;
   });
 
-  blocks.forEach((block) => {
-    block.addEventListener("click", () => {
-      if (!shuffled) {
-        return; // Prevent clicking on blocks before shuffling
-      }
+  function createBlocks(area, numBlocks) {
+    for (let i = 0; i < numBlocks; i++) {
+      const block = document.createElement("div");
+      block.className = "block";
+      const inner = document.createElement("div");
+      inner.className = "inner";
+      const img = document.createElement("img");
+      img.src = images[i];
+      img.alt = images[i].split("/").pop().split(".")[0];
+      inner.appendChild(img);
+      block.appendChild(inner);
+      area.appendChild(block);
+    }
+  }
 
-      if (!firstBlock) {
-        firstBlock = block;
-        firstBlock.classList.add("active");
-      } else if (!secondBlock) {
-        secondBlock = block;
-        secondBlock.classList.add("active");
-
-        // Check for a match
-        if (
-          firstBlock.querySelector("img").getAttribute("src") ===
-          secondBlock.querySelector("img").getAttribute("src")
-        ) {
-          // If it's a match, keep the blocks active
-          firstBlock = null;
-          secondBlock = null;
-        } else {
-          // If it's not a match, hide the images
-          setTimeout(() => {
-            firstBlock.classList.remove("active");
-            secondBlock.classList.remove("active");
-            firstBlock = null;
-            secondBlock = null;
-          }, 1000);
-        }
-      }
-    });
-  });
-
-  const startButton = document.getElementById("start-button");
-  startButton.addEventListener("click", () => {
-    // Shuffle the blocks/images
-    blocks.forEach((block) => {
-      block.classList.remove("active");
-    });
-    shuffleBlocks();
-    shuffled = true;
-  });
-
-  function shuffleBlocks() {
+  function shuffleBlocks(area) {
+    const blocks = area.querySelectorAll(".block");
     blocks.forEach((block) => {
       block.style.order = Math.floor(Math.random() * blocks.length);
     });
   }
+
+  playerArea.addEventListener("click", (event) => {
+    const clickedBlock = event.target.closest(".block");
+    if (!clickedBlock) return; // Ignore clicks on non-block elements
+    if (clickedBlock.classList.contains("active")) return; // Ignore clicks on active blocks
+
+    if (!firstBlock) {
+      firstBlock = clickedBlock;
+      firstBlock.classList.add("active");
+    } else if (!secondBlock) {
+      secondBlock = clickedBlock;
+      secondBlock.classList.add("active");
+
+      // Check for a match
+      if (
+        firstBlock.querySelector("img").getAttribute("src") ===
+        secondBlock.querySelector("img").getAttribute("src")
+      ) {
+        // If it's a match, keep the blocks active
+        firstBlock = null;
+        secondBlock = null;
+      } else {
+        // If it's not a match, hide the images
+        setTimeout(() => {
+          firstBlock.classList.remove("active");
+          secondBlock.classList.remove("active");
+          firstBlock = null;
+          secondBlock = null;
+        }, 1000);
+      }
+    }
+  });
 });
