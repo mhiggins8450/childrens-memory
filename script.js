6edd226cab7c91a8c274a064aa870b0699f9cfd3
mhiggins8450 <146852125+mhiggins8450@users.noreply.github.com>
2023-11-11 10:58:12 -0500

    Reverted back to previous commit; considering revamping entire gameplay

--- script.js
+++ script.js
@@ -3,74 +3,68 @@ document.addEventListener("DOMContentLoaded", function () {
   const resetButton = document.getElementById("reset-button");
   const easyButton = document.getElementById("easy-button");
   const hardButton = document.getElementById("hard-button");
-  let currentPlayer = "player1";
+  let currentPlayer = "player";
   let shuffled = false;
   let firstBlock = null;
   let secondBlock = null;
-  let player1Pairs = 0;
-  let player2Pairs = 0;
+  let playerPairs = 0;
+  let computerPairs = 0;
   const images = [
-    "images/cheetah.jpg",
     "images/deer.jpg",
-    "images/ducks.jpg",
-    "images/elephant.jpg",
     "images/fox.jpg",
     "images/giraffe.jpg",
     "images/hedgehog.jpg",
-    "images/kaola.jpg",
-    "images/kittens.jpg",
-    "images/lions.jpg",
-    "images/meerkats.jpg",
-    "images/owl.jpg",
+    "images/polarbear.jpg",
+    "images/rabbit.jpg",
+    "images/deer.jpg",
+    "images/fox.jpg",
+    "images/giraffe.jpg",
+    "images/hedgehog.jpg",
+    "images/polarbear.jpg",
+    "images/rabbit.jpg",
   ];
 
+  // Shuffle the unpaired images array
   function shuffleUnpairedImages() {
     const unpairedImages = images.slice(0, images.length / 2);
     for (let i = unpairedImages.length - 1; i > 0; i--) {
       const j = Math.floor(Math.random() * (i + 1));
-      [unpairedImages[i], unpairedImages[j]] = [
-        unpairedImages[j],
-        unpairedImages[i],
-      ];
+      [unpairedImages[i], unpairedImages[j]] = [unpairedImages[j], unpairedImages[i]];
     }
     return [...unpairedImages, ...unpairedImages];
   }
 
-  function easyButtonHandler() {
+  easyButton.addEventListener("click", () => {
     if (!shuffled) {
       gameArea.innerHTML = "";
       createBlocks(gameArea, 24, true);
       shuffled = true;
     }
-  }
+  });
 
-  function hardButtonHandler() {
+  hardButton.addEventListener("click", () => {
     if (!shuffled) {
       gameArea.innerHTML = "";
       createBlocks(gameArea, 24, true);
       shuffled = true;
     }
-  }
+  });
 
-  function resetButtonHandler() {
+  resetButton.addEventListener("click", () => {
     gameArea.innerHTML = "";
     shuffled = false;
-    currentPlayer = "player1";
-    player1Pairs = 0;
-    player2Pairs = 0;
+    currentPlayer = "player";
+    playerPairs = 0;
+    computerPairs = 0;
     updatePairs();
-  }
+  });
 
   function createBlocks(area, numBlocks, shuffleAll) {
-    const shuffledImages = shuffleAll
-      ? shuffleImages(images)
-      : shuffleUnpairedImages();
-    const duplicatedBlocks = shuffleAll
-      ? shuffleImages(shuffledImages)
-      : shuffleUnpairedImages();
-    const allImages = shuffleAll
-      ? [...shuffledImages, ...duplicatedBlocks]
-      : shuffleUnpairedImages();
+    const shuffledImages = shuffleAll ? shuffleImages(images) : shuffleUnpairedImages();
+
+    // Duplicate the blocks for the second set
+    const duplicatedBlocks = shuffleAll ? shuffledImages : shuffleUnpairedImages();
+    const allImages = shuffleAll ? [...shuffledImages, ...duplicatedBlocks] : shuffledImages;
 
     for (let i = 0; i < numBlocks; i++) {
       const block = document.createElement("div");
@@ -85,7 +79,7 @@ document.addEventListener("DOMContentLoaded", function () {
       area.appendChild(block);
 
       block.addEventListener("click", () => {
-        if (currentPlayer === "player1" || currentPlayer === "player2") {
+        if (currentPlayer === "player") {
           handleBlockClick(block);
         }
       });
@@ -111,20 +105,18 @@ document.addEventListener("DOMContentLoaded", function () {
       secondBlock = clickedBlock;
       secondBlock.classList.add("active");
 
-      if (
-        firstBlock.querySelector("img").getAttribute("src") ===
-        secondBlock.querySelector("img").getAttribute("src")
-      ) {
+      if (firstBlock.querySelector("img").getAttribute("src") ===
+        secondBlock.querySelector("img").getAttribute("src")) {
         if (firstBlock.dataset.player === "true") {
-          player1Pairs++;
+          playerPairs++;
         } else {
-          player2Pairs++;
+          computerPairs++;
         }
 
         firstBlock = null;
         secondBlock = null;
 
-        if (player1Pairs + player2Pairs === 12) {
+        if (playerPairs + computerPairs === 12) {
           displayResult();
         }
 
@@ -135,25 +127,74 @@ document.addEventListener("DOMContentLoaded", function () {
           secondBlock.classList.remove("active");
           firstBlock = null;
           secondBlock = null;
-          currentPlayer = currentPlayer === "player1" ? "player2" : "player1";
+          currentPlayer = "computer";
+          simulateComputerTurn();
         }, 1000);
       }
     }
   }
 
+  function simulateComputerTurn() {
+    const hiddenBlocks = Array.from(
+      gameArea.querySelectorAll(".block:not(.active)")
+    );
+
+    const selectedIndices = [];
+    while (selectedIndices.length < 2) {
+      const randomIndex = Math.floor(Math.random() * hiddenBlocks.length);
+      if (!selectedIndices.includes(randomIndex)) {
+        selectedIndices.push(randomIndex);
+      }
+    }
+
+    selectedIndices.forEach((index) => {
+      const block = hiddenBlocks[index];
+      block.classList.add("active");
+    });
+
+    setTimeout(() => {
+      const activeBlocks = Array.from(
+        gameArea.querySelectorAll(".block.active")
+      );
+
+      if (
+        activeBlocks[0].querySelector("img").getAttribute("src") ===
+        activeBlocks[1].querySelector("img").getAttribute("src")
+      ) {
+        if (activeBlocks[0].dataset.player === "true") {
+          playerPairs++;
+        } else {
+          computerPairs++;
+        }
+
+        activeBlocks.forEach((block) => block.classList.remove("active"));
+      } else {
+        setTimeout(() => {
+          activeBlocks.forEach((block) => block.classList.remove("active"));
+          currentPlayer = "player";
+          updatePairs();
+        }, 1000);
+      }
+
+      if (playerPairs + computerPairs === 12) {
+        displayResult();
+      }
+    }, 1000);
+  }
+
   function updatePairs() {
-    const player1Title = document.getElementById("player1-title");
-    const player2Title = document.getElementById("player2-title");
-    player1Title.innerHTML = `Player 1 Area<br>Pairs: ${player1Pairs}`;
-    player2Title.innerHTML = `Player 2 Area<br>Pairs: ${player2Pairs}`;
+    const playerTitle = document.getElementById("player-title");
+    const computerTitle = document.getElementById("computer-title");
+    playerTitle.innerHTML = `Player Area<br>Pairs: ${playerPairs}`;
+    computerTitle.innerHTML = `Computer Area<br>Pairs: ${computerPairs}`;
   }
 
   function displayResult() {
     let result = "";
-    if (player1Pairs > player2Pairs) {
-      result = "Player 1 Wins!";
-    } else if (player2Pairs > player1Pairs) {
-      result = "Player 2 Wins!";
+    if (playerPairs > computerPairs) {
+      result = "You Win!";
+    } else if (computerPairs > playerPairs) {
+      result = "The Computer Wins!";
     } else {
       result = "It's a Tie!";
     }
@@ -164,8 +205,6 @@ document.addEventListener("DOMContentLoaded", function () {
     message.style.fontWeight = "bold";
     message.style.marginTop = "20px";
     gameArea.appendChild(message);
-
-    currentPlayer = "player1";
 
     const playAgainButton = document.createElement("button");
     playAgainButton.innerText = "Play Again";
@@ -180,20 +219,13 @@ document.addEventListener("DOMContentLoaded", function () {
     playAgainButton.addEventListener("click", () => {
       gameArea.innerHTML = "";
       createBlocks(gameArea, 24, shuffled);
-      currentPlayer = "player1";
-      player1Pairs = 0;
-      player2Pairs = 0;
+      currentPlayer = "player";
+      playerPairs = 0;
+      computerPairs = 0;
       updatePairs();
       message.remove();
       playAgainButton.remove();
     });
     gameArea.appendChild(playAgainButton);
   }
-
-  easyButton.addEventListener("click", easyButtonHandler);
-  hardButton.addEventListener("click", hardButtonHandler);
-  resetButton.addEventListener("click", resetButtonHandler);
-
-  // Initial setup for the game
-  createBlocks(gameArea, 24, false);
 });
