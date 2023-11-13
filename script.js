1c5dd5af11e83d36f03da6fe9fa8ff7dd8d2b0c4
mhiggins8450 <146852125+mhiggins8450@users.noreply.github.com>
2023-11-12 19:11:47 -0500

    Added comments to help explain code logic and flow

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
+// Wait for the DOM content to be fully loaded before executing the code
 document.addEventListener("DOMContentLoaded", function () {
+  // Get references to various elements in the HTML document
   const gameArea = document.querySelector(".game-area");
   const pairingCount = document.getElementById("pairing-count");
   const resultMessage = document.getElementById("result-message");
   const player1Score = document.getElementById("player1");
   const player2Score = document.getElementById("player2");
+
+  // Initialize game variables
   let currentPlayer = "player1";
   let firstBlock = null;
   let secondBlock = null;
   let player1Pairs = 0;
   let player2Pairs = 0;
 
+  // Array of image sources for the memory game
   const images = [
     "images/elephant.jpg",
     "images/polarbear.jpg",
@@ -21,14 +26,17 @@ document.addEventListener("DOMContentLoaded", function () {
     "images/rabbit.jpg",
   ];
 
+  // Function to shuffle the images for a random game layout
   function shuffleImages() {
     return [...images, ...images].sort(() => Math.random() - 0.5);
   }
 
+  // Function to create the memory game blocks
   function createBlocks() {
     const shuffledImages = shuffleImages();
 
-    gameArea.innerHTML = ""; // Clear the game area before creating new blocks
+    // Clear the game area before creating new blocks
+    gameArea.innerHTML = "";
 
     shuffledImages.forEach((image, index) => {
       const block = document.createElement("div");
@@ -42,11 +50,14 @@ document.addEventListener("DOMContentLoaded", function () {
       block.appendChild(inner);
       gameArea.appendChild(block);
 
+      // Add a click event listener to each block
       block.addEventListener("click", () => handleBlockClick(block, index));
     });
   }
 
+  // Function to handle a block click event
   function handleBlockClick(clickedBlock, index) {
+    // Check if the clicked block is active and no blocks are currently selected
     if (!clickedBlock.classList.contains("active") && firstBlock === null) {
       clickedBlock.classList.add("active");
       firstBlock = { block: clickedBlock, index };
@@ -54,11 +65,14 @@ document.addEventListener("DOMContentLoaded", function () {
       clickedBlock.classList.add("active");
       secondBlock = { block: clickedBlock, index };
 
+      // Check for a pair after the second block is selected
       checkForPair();
     }
   }
 
+  // Function to check if the selected blocks form a pair
   function checkForPair() {
+    // Check if both blocks are selected
     if (!firstBlock || !secondBlock) {
       return;
     }
@@ -66,87 +80,113 @@ document.addEventListener("DOMContentLoaded", function () {
     const firstImage = firstBlock.block.querySelector("img");
     const secondImage = secondBlock.block.querySelector("img");
 
+    // Check if the images of the selected blocks are the same
     if (firstImage.src === secondImage.src) {
-      // It's a pair
+      // Increment the pair count for the current player
       if (currentPlayer === "player1") {
         player1Pairs++;
       } else {
         player2Pairs++;
       }
+
+      // Clear the selected blocks
       firstBlock = null;
       secondBlock = null;
 
       // Check for win-state or tie-state
       if (player1Pairs + player2Pairs === 8) {
         if (player1Pairs > player2Pairs) {
+          // Display a win message for Player 1
           displayWinMessage("Player 1");
         } else if (player2Pairs > player1Pairs) {
+          // Display a win message for Player 2
           displayWinMessage("Player 2");
         } else {
+          // Display a tie message
           displayTieMessage();
         }
       } else {
+        // Switch to the next player's turn
         switchPlayer();
       }
     } else {
-      // Not a pair
+      // Not a pair, wait for a short duration before hiding the blocks
       setTimeout(() => {
         firstBlock.block.classList.remove("active");
         secondBlock.block.classList.remove("active");
         firstBlock = null;
         secondBlock = null;
+
+        // Switch to the next player's turn
         switchPlayer();
       }, 1000);
     }
 
+    // Update the display of pairs and the active player
     updatePairs();
   }
 
+  // Function to display a win message
   function displayWinMessage(winner) {
     alert(`${winner} Wins!`);
+    // Reset the game after displaying the message
     resetGame();
   }
 
+  // Function to display a tie message
   function displayTieMessage() {
     alert("It's a Tie!");
+    // Reset the game after displaying the message
     resetGame();
   }
 
+  // Function to switch to the next player's turn
   function switchPlayer() {
+    // Toggle between Player 1 and Player 2
     currentPlayer = currentPlayer === "player1" ? "player2" : "player1";
+    // Update the active player indicator
     updateActivePlayerIndicator();
   }
 
+  // Function to update the display of pairs for each player
+  function updatePairs() {
+    // Get references to the elements displaying player pairs
+    const player1Title = document.getElementById("player1");
+    const player2Title = document.getElementById("player2");
+
+    // Update the text content with the current pair counts
+    if (player1Title && player2Title) {
+      player1Title.innerHTML = `Player 1 Pairs: ${player1Pairs}`;
+      player2Title.innerHTML = `Player 2 Pairs: ${player2Pairs}`;
+    }
+  }
+
+  // Function to update the active player indicator
   function updateActivePlayerIndicator() {
     const player1Indicator = document.getElementById("player1");
     const player2Indicator = document.getElementById("player2");
 
+    // Remove the "active-player" class from both indicators
     player1Indicator.classList.remove("active-player");
     player2Indicator.classList.remove("active-player");
 
+    // Add the "active-player" class to the indicator of the current player
     if (currentPlayer === "player1") {
-        player1Indicator.classList.add("active-player");
+      player1Indicator.classList.add("active-player");
     } else {
-        player2Indicator.classList.add("active-player");
+      player2Indicator.classList.add("active-player");
     }
-}
+  }
 
-  function updatePairs() {
-    const player1Title = document.getElementById("player1");
-    const player2Title = document.getElementById("player2");
-
-    if (player1Title && player2Title) {
-        player1Title.innerHTML = `Player 1 Pairs: ${player1Pairs}`;
-        player2Title.innerHTML = `Player 2 Pairs: ${player2Pairs}`;
-    }
-}
-
+  // Function to reset the game state
   function resetGame() {
+    // Reset pair counts, current player, and create new blocks
     player1Pairs = 0;
     player2Pairs = 0;
     currentPlayer = "player1";
     createBlocks();
   }
 
+  // Initial creation of game blocks when the page is loaded
   createBlocks();
 });
